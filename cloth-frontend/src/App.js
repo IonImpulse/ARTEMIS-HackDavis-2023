220537467cdde230b74cbdbb7972345aedc2ac19
BuildTools <unconfigured@null.spigotmc.org>
2023-05-20 14:06:01 -0700

    Display cart

--- cloth-frontend/src/App.js
+++ cloth-frontend/src/App.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import './App.css';
+import Cart from './components/Cart.js';
 import LoginScreen from './components/LoginScreen.js';
 
 export default function App() {
   return (
     <div>
       <LoginScreen />
+      <Cart cart={{backpacks:5, shirts:20, pants:80}}/>
     </div>
   );
 }
