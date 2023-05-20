55a0357d0cab128ac4b6b58afb8ad60206b7a90b
Ethan Vazquez <edv121@outlook.com>
2023-05-20 15:57:14 -0700

    up

--- cloth-frontend/src/components/ItemSelector.js
+++ cloth-frontend/src/components/ItemSelector.js
@@ -6,20 +6,27 @@ export default function ItemSelector(props) {
 
     const { availableItems, cart, addToCart } = props;
 
+    let buttons = [];
+    for (let i = 0; i < availableItems.length; i++) {
+        let item = availableItems[i];
+        let src = `/ItemIcons/${item.name}.png`;
+
+        buttons.push(
+            <Button
+                key={item.id}
+                variant="contained"
+                onClick={() => addToCart(item)}>
+                <img src="" alt="buttonpng" border="0" />
+                {item.name}
+            </Button>
+        );
+    }
     return (
         <>
             <div className="ItemSelector">
                 <h1>Items</h1>
                 <div className="Items">
-                    {availableItems.map((item) => (
-                        <Button
-                            key={item.id}
-                            variant="contained"
-                            onClick={() => addToCart(item)}>
-                            <img src="ItemIcons/Backpack.jpg" alt="buttonpng" border="0" />
-                            {item.name}
-                        </Button>
-                    ))}
+                    {buttons}
                 </div>
             </div>
         </>
